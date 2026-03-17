907d58e79efde56790862757e16d14ceba334b2b
Leaves-ming <549012861@qq.com>
2026-03-17 20:16:29 +0800

    项目初始化

---
+++ im-chat-server/src/main/java/com/ming/imchatserver/ImChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.ming.imchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImChatServerApplication.class, args);
+    }
+
+}
---
+++ im-chat-server/src/test/java/com/ming/imchatserver/ImChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.imchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
